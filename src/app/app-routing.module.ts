bd8460af0eed9158c4829d6234de1ca6a5cf34ec
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2022-10-11 19:45:33 -0500

    [ADD] screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SeguridadModule } from './pages/seguridad/seguridad.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SeguridadModule,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
