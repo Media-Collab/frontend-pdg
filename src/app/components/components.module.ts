02b6311301cbf0282a45d45a8b5f72bed02873bd
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2022-10-19 21:43:43 -0500

    [ADD] us detailts

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CardBlogComponent } from './card-blog/card-blog.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, CardBlogComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent, CardBlogComponent],
 })
 export class ComponentsModule {}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,17 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { UsComponent } from './us/us.component';
 import { BlogComponent } from './blog/blog.component';
-
-
+// import components
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
-  declarations: [
-    MainComponent,
-    UsComponent,
-    BlogComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [MainComponent, UsComponent, BlogComponent],
+  imports: [CommonModule, ComponentsModule],
 })
-export class HomeModule { }
+export class HomeModule {}
