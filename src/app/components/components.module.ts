47a483314467d6c94da557d8c3e4f286b4abe2dd
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2022-10-24 15:18:16 -0500

    [ADD] responsive in header

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,10 +4,29 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CardBlogComponent } from './card-blog/card-blog.component';
 import { RouterModule } from '@angular/router';
+import {
+  FooterModule,
+  HeaderModule,
+  DropdownModule,
+  GridModule,
+  NavModule,
+} from '@coreui/angular';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, CardBlogComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    FooterModule,
+    HeaderModule,
+    DropdownModule,
+    NavModule,
+    GridModule,
+    MatIconModule,
+    MatToolbarModule,
+    RouterModule,
+  ],
   exports: [HeaderComponent, FooterComponent, CardBlogComponent],
 })
 export class ComponentsModule {}
