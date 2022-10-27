fc407386a08aee60714414e27114efa8800455ba
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2022-10-26 22:41:16 -0500

    [ADD] component activity

--- src/app/pages/test/test.module.ts
+++ src/app/pages/test/test.module.ts
@@ -4,9 +4,21 @@ import { TestComponent } from './test/test.component';
 import { FormsModule } from '@angular/forms';
 import { EstudiantesModule } from '../estudiantes/estudiantes.module';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { CreateActivityComponent } from './create-activity/create-activity.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [TestComponent],
-  imports: [CommonModule, FormsModule, ComponentsModule, EstudiantesModule],
+  declarations: [TestComponent, CreateActivityComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ComponentsModule,
+    EstudiantesModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
 })
 export class TestModule {}
